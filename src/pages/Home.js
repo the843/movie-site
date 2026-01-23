9d455ffeb6cf1cc9fa30fb35cc738a882bbbddf3
the843 <faikohicham15@gmail.com>
2026-01-23 19:19:07 +0100

    Fix unused variable ESLint error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { getTrendingMovies,  getLatestMovies, getPopularSeries } from "../api";
+
 import "./Home.css";
 
 export default function Home() {
