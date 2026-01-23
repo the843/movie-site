68fa503f33be213deac9f9d51ec4f2c13b3a9fa6
the843 <faikohicham15@gmail.com>
2026-01-23 19:15:43 +0100

    Remove unused getPopularMovies and fix Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { getTrendingMovies, getPopularMovies, getLatestMovies, getPopularSeries } from "../api";
+import { getTrendingMovies,  getLatestMovies, getPopularSeries } from "../api";
 import "./Home.css";
 
 export default function Home() {
