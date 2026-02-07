96d864865f6c7591f82b750e0c51caa6adae6435
the843 <faikohicham15@gmail.com>
2026-02-07 18:21:08 +0100

    Fix sitemap routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MovieDetails from "./pages/MovieDetails";
@@ -5,8 +6,22 @@ import Trending from "./pages/Trending";
 import Movies from "./pages/Movies";
 import Series from "./pages/Series";
 
-
 function App() {
+  useEffect(() => {
+    // دمج Monetag Multitag
+    const script = document.createElement("script");
+    script.src = "https://quge5.com/88/tag.min.js";  // الكود اللي عطاك Monetag
+    script.async = true;
+    script.setAttribute("data-zone", "204828");
+    script.setAttribute("data-cfasync", "false");
+    document.body.appendChild(script);
+
+    // تنظيف الكود إذا تم إزالة Component
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
